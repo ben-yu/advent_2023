f00b7cd1f64f49504777c6a4f5acf98eed7dfaf5
benjamyu <benjamyu@twitch.tv>
2023-12-01 22:33:12 -0800

    Day 2

---
+++ src/day2_1.rs
@@ -0,0 +1,43 @@
+use anyhow::Result;
+use std::io::BufRead;
+
+fn main() -> Result<()> {
+    let stdin = std::io::stdin();
+    let mut sum = 0;
+    for line in stdin.lock().lines() {
+
+        let line = line?;
+        let colon_split: Vec<&str> = line.split(":").collect();
+        let game_string: Vec<&str> = colon_split[0].split(" ").collect();
+        let game_id = game_string[1].parse::<u32>().unwrap();
+        let games = colon_split[1].split(";");
+        let mut max_red: u32 = 0;
+        let mut max_green: u32 = 0;
+        let mut max_blue: u32 = 0;
+
+        for game in games {
+            let g_str: Vec<&str> = game.split(",").collect();
+            for dice_str in g_str {
+                let s: Vec<&str> = dice_str.trim().split(" ").collect();
+                let dice_count = s[0].trim().parse::<u32>().unwrap();
+                let dice_color = s[1].trim();
+
+                if dice_color == "green" && max_green < dice_count {
+                    max_green = dice_count;
+                } else if dice_color == "red" && max_red < dice_count {
+                    max_red = dice_count;
+                } else if dice_color == "blue" && max_blue < dice_count {
+                    max_blue = dice_count;
+                }
+            }
+        }
+
+        if max_red <= 12 && max_green <= 13 && max_blue <= 14 {
+            sum += game_id;
+            println!("{}", game_id)
+        }
+
+    }
+    println!("Answer: {}", sum);
+    Ok(())
+}
